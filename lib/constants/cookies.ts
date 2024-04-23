8eacd19b567b14ee9fca16b36ca69399defeb3aa
Janning <141676057+janning-kontent@users.noreply.github.com>
2024-04-22 20:35:24 -0500

    Initialize merge/main_menu-fullwidth_banner-fullwidth

---
+++ lib/constants/cookies.ts
@@ -0,0 +1,7 @@
+export const personaCookieName = "currentPersona";
+
+export const envIdCookieName = "currentEnvId";
+
+export const urlAfterAuthCookieName = "urlAfterAuth";
+
+export const defaultCookieOptions = { path: '/', sameSite: 'none', secure: true } as const;
